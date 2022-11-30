7e2d7b24a2a597c9156c45b3ead3cdf9ae16aa2c
Théo Friberg <fsmnarmosta@posteo.net>
2022-11-30 12:58:28 +0200

    skeleton of the model

--- client/model.py
+++ client/model.py
@@ -1,1 +1,28 @@
 # keeps track of game state
+
+class Player():
+    pass
+
+class Grid():
+    pass
+
+class Ruutu():
+    pass
+
+class Model():
+
+    def __init__():
+        self.grid = Grid()
+        self.players = []
+
+    def update(event):
+        pass
+
+    def lock():
+        pass
+
+    def unlock():
+        pass
+
+    def read(index):
+        pass
