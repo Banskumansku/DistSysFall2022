039265c662c007c9f34137b79513baad636d5fe4
Théo Friberg <fsmnarmosta@posteo.net>
2022-12-12 21:32:20 +0200

    make mock_matchmaker reply json as specified

--- client/mock_matchmaker.py
+++ client/mock_matchmaker.py
@@ -6,15 +6,15 @@ def reply(environ, start_response):
     print("wsgi.input", environ["wsgi.input"].read())
     if environ["PATH_INFO"] == "/request-match" and environ["REQUEST_METHOD"] == "POST":
         status = '200 OK'
-        headers = [('Content-type', 'text/plain; charset=utf-8')]
+        headers = [('Content-type', 'application/json; charset=utf-8')]
         start_response(status, headers)
-        return ["success".encode("utf-8"),]
+        return ['{"status":"success"}'.encode("utf-8"),]
 
 
     status = '404 NOT FOUND'
-    headers = [('Content-type', 'text/plain; charset=utf-8')]
+    headers = [('Content-type', 'application/json; charset=utf-8')]
     start_response(status, headers)
-    return ["Not Found".encode("utf-8"),]
+    return ['{"error": "not found"}'.encode("utf-8"),]
 
 cherrypy.config.update({'server.socket_port': 1337})
 
