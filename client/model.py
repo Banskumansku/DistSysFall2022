21d97bee81414e1a632402433a944387a9df5a15
Théo Friberg <fsmnarmosta@posteo.net>
2022-12-14 22:15:00 +0200

    thoroughly comment the model

--- client/model.py
+++ client/model.py
@@ -7,24 +7,22 @@ from eventmanager import EventManager, ReadBoardEvent, UpdateBoardEvent, BoardSt
 
 from enum import Enum
 
-class Player():
-    pass
-
 class Button:
-    """Create a button, then blit the surface in the while loop"""
  
-    def __init__(self, text,  pos, font, bg="black", feedback=""):
+    def __init__(self, text,  pos, font):
         self.x, self.y = pos
         self.font = pygame.font.SysFont("Sans", font)
         if feedback == "":
             self.feedback = "text"
         else:
             self.feedback = feedback
-        self.change_text(text, bg)
+        self.change_text(text, white)
  
-    def change_text(self, text, bg="black"):
-        """Change the text when you click"""
-        self.text = self.font.render(text, 1, pygame.Color("White"))
+    def change_text(self, text):
+
+        # Render the text of the button
+
+        self.text = self.font.render(text, 1, pygame.Color("Black"))
         self.size = self.text.get_size()
         self.surface = pygame.Surface(self.size)
         self.surface.fill(bg)
@@ -32,19 +30,29 @@ class Button:
         self.rect = pygame.Rect(self.x, self.y, self.size[0], self.size[1])
  
     def show(self, screen):
+
+        # Render the button onto something else
+
         screen.blit(self.surface, (self.x, self.y))
 
     def is_within_bounds(self, x, y):
+
+        # Check if click is in bounds
+
         if self.x > x or self.y > y:
             return False
         if self.x + self.size[0] < x or self.y + self.size[1] < y:
             return False
         return True
 
+# Enum representing a square
+
 class Ruutu(Enum):
     EMPTY = 0
     NOUGHT = 1
     CROSS = 2
+
+# The state of the game
 
 class Model():
 
@@ -52,6 +60,9 @@ class Model():
         self.reset_state()
 
     def reset_state(self):
+
+        # A blank grid
+
         self.grid = []
         self.players = []
         for y in range(3):
@@ -60,12 +71,18 @@ class Model():
                 row.append(Ruutu.EMPTY)
             self.grid.append(row)
 
+    # Register to the event manager and keep a handle to it
+
     def set_event_manager(self, event_manager):
         self.event_manager = event_manager
         self.event_manager.RegisterListener(self)
 
+    # List of the end points of the winning rows
+
     def winning_rows(self):
         result = []
+
+        # Horizontal
 
         for hline in range(3):
             k = self.grid[hline][0]
@@ -74,6 +91,8 @@ class Model():
             if k == self.grid[hline][1] and k == self.grid[hline][2]:
                 result.append([[0, hline], [2, hline]])
 
+        # Vertical
+
         for vline in range(3):
             k = self.grid[0][vline]
             if k == Ruutu.EMPTY:
@@ -81,8 +100,12 @@ class Model():
             if k == self.grid[1][vline] and k == self.grid[2][vline]:
                 result.append([[vline, 0], [vline, 2]])
 
+        # Diagonal 1
+
         if self.grid[0][0] != Ruutu.EMPTY and self.grid[0][0] == self.grid[1][1] and self.grid[0][0] == self.grid[2][2]:
             result.append([[0, 0], [2, 2]])
+
+        # Diagonal 2
 
         if self.grid[2][0] != Ruutu.EMPTY and self.grid[2][0] == self.grid[1][1] and self.grid[2][0] == self.grid[0][2]:
             result.append([[2, 0], [0, 2]])
@@ -91,12 +114,19 @@ class Model():
 
     def notify(self, event):
         
+        # Inform the world about the board upon request
+
         if isinstance(event, ReadBoardEvent):
             self.event_manager.Post(BoardStateEvent(copy.deepcopy(self.grid), self.winning_rows()))
         
+        # Update the state of the board
+
         if isinstance(event, UpdateBoardEvent):
             self.grid[event.y][event.x] = event.payload
             self.event_manager.Post(BoardStateEvent(copy.deepcopy(self.grid), self.winning_rows()))
+
+        # Reset the board
+
         if isinstance(event, ResetBoardEvent):
             self.reset_state()
             self.event_manager.Post(BoardStateEvent(copy.deepcopy(self.grid), self.winning_rows()))
