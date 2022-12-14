e6a7585ff251eb0bf1e825e06f615757134d0420
Théo Friberg <fsmnarmosta@posteo.net>
2022-12-14 10:25:29 +0200

    deduce port from return url

--- client/main.py
+++ client/main.py
@@ -8,7 +8,7 @@ import sys
 
 
 try:
-    context = {"MATCHMAKER": sys.argv[1], "RETURN_ADDRESS": sys.argv[2], "NAME": sys.argv[3]}
+    context = {"MATCHMAKER": sys.argv[1], "RETURN_ADDRESS": sys.argv[2], "NAME": sys.argv[3], "PORT": int(sys.argv[2].split(":")[2])}
 except IndexError:
     print("usage: main.py <matchmaker address> <return address> <name>")
     sys.exit(1)
@@ -26,7 +26,7 @@ v.set_event_manager(e)
 
 s = Server()
 s.set_event_manager(e)
-s.serve(8080)
+s.serve(context["PORT"])
 
 e.StartNotifying()
 
