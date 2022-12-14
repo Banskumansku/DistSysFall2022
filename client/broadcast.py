ac10e5c9e2726447a561ce0fd0b5a31d01878138
Théo Friberg <fsmnarmosta@posteo.net>
2022-12-14 21:49:21 +0200

    thoroughly comment broadcaster

--- client/broadcast.py
+++ client/broadcast.py
@@ -1,3 +1,6 @@
+# The broadcaster is in charge of making requests outwards
+# In a more real-worldey implementation: handle multicasting
+
 from eventmanager import EventManager, BroadcastEvent, ReplyEvent
 import requests
 
@@ -5,21 +8,33 @@ class Broadcaster():
     def __init__(self):
         pass
 
+    # Register and get a handle on the event manager
+
     def set_event_manager(self, event_manager):
         self.event_manager = event_manager
         self.event_manager.RegisterListener(self)
 
     def notify(self, event):
+
+        # Only react to BroadcastEvents
+
         if isinstance(event, BroadcastEvent):
             print("--- Commence broadcast ---")
             print(f"Target: {event.target}")
             print(f"Payload: {event.payload}")
             print("------ End broadcast -----")
             print("----- Commence reply -----")
+
+            # Catch network errors
+
             try:
                 result = requests.post(event.target, event.payload).json()
-            except:
+            except: # <- bad form, but justified by variety of potential errors
                 result = None
+
             print(result)
             print("------- End reply --------")
+
+            # Tell the event system of whatever the other end said
+
             self.event_manager.Post(ReplyEvent(event.target, result))
