7fb9b0d606e9d7c68422d7820053ccab25edf473
Théo Friberg <fsmnarmosta@posteo.net>
2022-12-14 22:14:44 +0200

    thoroughly comment main

--- client/main.py
+++ client/main.py
@@ -1,3 +1,5 @@
+# Import all the disparate part of the system
+
 from view import ViewManager
 from eventmanager import EventManager
 from broadcast import Broadcaster
@@ -5,14 +7,17 @@ from controller import Controller
 from server import Server
 from model import Model
 
+# Read cli parameters
+
 import sys
-
-
 try:
     context = {"MATCHMAKER": sys.argv[1], "RETURN_ADDRESS": sys.argv[2], "NAME": sys.argv[3], "PORT": int(sys.argv[2].split(":")[2])}
 except IndexError:
     print("usage: main.py <matchmaker address> <return address> <name>")
     sys.exit(1)
+
+# - Initialize the various parts of the program
+# - Hook them up to the event manager
 
 e = EventManager()
 
@@ -30,8 +35,15 @@ m.set_event_manager(e)
 
 s = Server()
 s.set_event_manager(e)
+
+# Start the server
+
 s.serve(context["PORT"])
 
+# Start the event manager's loop
+
 e.StartNotifying()
+
+# Launch the Pygame part of the game
 
 v.main_game()
